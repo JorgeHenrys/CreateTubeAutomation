e24aa9660995c861ed51948293ffbfd669b9897a
Jorge <jorgehenriq23121996@gmail.com>
2019-09-18 16:28:05 -0300

    feat: render video with After Effects

--- robots/state.js
+++ robots/state.js
@@ -1,9 +1,17 @@
 const fs = require('fs')
 const contentFilePath = './content.json'
+const scriptFilePath = './content/after-effects-script.js'
 
 function save(content) {
 	const contentString = JSON.stringify(content)
 	return fs.writeFileSync(contentFilePath, contentString)
+
+}
+
+function saveScript(content) {
+	const contentString = JSON.stringify(content)
+	const scriptString = `var content = ${contentString}`
+	return fs.writeFileSync(scriptFilePath, scriptString)
 
 }
 
@@ -16,5 +24,6 @@ function load() {
 
 module.exports = {
 	save,
+	saveScript,
 	load
 }
