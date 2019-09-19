7f3a90b6e65fdc675b869d03e97062bb2c5e51ca
Jorge <jorgehenriq23121996@gmail.com>
2019-09-19 02:00:16 -0300

    feat: final adjustament

--- robots/text.js
+++ robots/text.js
@@ -15,6 +15,7 @@ const state = require('./state.js')
 
 
 async function robot() {
+	console.log('> [text-robot] Starting...')
 	const content = state.load()
 
 	await fetchContentFromWikipedia(content)
@@ -27,6 +28,7 @@ async function robot() {
 
 
 	async function fetchContentFromWikipedia(content) {
+		console.log('> [text-robot] Fetching content from Wikipedia')
 		const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
 		const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
 		const wikipediaResponse = await wikipediaAlgorithm.pipe({
@@ -38,6 +40,7 @@ async function robot() {
 		
 
 		content.sourceContentOriginal = wikipediaContent.content
+		console.log('> [text-robot] Fetching done!')
 	}
 
 	function sanitizeContent(content) {
@@ -84,8 +87,12 @@ async function robot() {
   	}
 
   	async function fetchKeywordsOfAllSentences(content) {
+  		console.log('> [text-robot] Starting to fetch keywords from Watson')
   		for (const sentence of content.sentences) {
+  			console.log(`> [text-robot] Sentence: "${sentence.text}"`)
   			sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+
+  			console.log(`> [text-robot] Keywords: ${sentence.keywords.join(', ')}\n`)
   		}
   	}
 
